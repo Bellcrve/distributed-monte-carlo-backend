f70f6c81b6b43d9c5a2d704d7c16b82b1cc94d0d
tim506877 <tim506877@gmail.com>
2024-10-05 20:10:36 -0400

    Open web socket to send data back to front end

--- app/main.py
+++ app/main.py
@@ -3,8 +3,9 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
 from api.routes import api_router
+from websocket.handlers import web_router
 
 app = FastAPI()
 
 app.include_router(api_router)
-
+app.include_router(web_router)
